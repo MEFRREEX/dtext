8abd1d4b6b660a35ce4fba3b201c0f0dbd71666d
MEFRREEX <mefrreex@gmail.com>
2023-11-14 07:01:27 +0200

    Refactor

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,53 +1,23 @@
-function process() {
-    const input = document.getElementById("input").value;
-    let result;
+function processConvert() {
+    const inputElement = document.getElementById("input");
+    const resultElement = document.getElementById("result");
+    
+    const input = inputElement.value.trim();
+    
     if (input === "") {
-      result = "Заполните поле выше, оно не может быть пустым!";
+        resultElement.innerHTML = "Заполните поле выше, оно не может быть пустым!";
     } else {
-      result = replace(input);
+        const result = replace(input);
+        resultElement.innerHTML = result;
     }
-    document.getElementById("result").innerHTML = result;
 }
-  
-function replace(str) {
-    return str.toLowerCase()
-        .replaceAll("а", "뜀").replaceAll("б", "뜁")
-        .replaceAll("в", "뜂").replaceAll("г", "뜃")
-        .replaceAll("д", "뜄").replaceAll("е", "뜅")
-        .replaceAll("ё", "뜅").replaceAll("ж", "뜆")
-        .replaceAll("з", "뜇").replaceAll("и", "뜈")
-        .replaceAll("й", "뜉").replaceAll("к", "뜊")
-        .replaceAll("л", "뜋").replaceAll("м", "뜌")
-        .replaceAll("н", "뜍").replaceAll("о", "뜎")
-        .replaceAll("п", "뜏").replaceAll("р", "뜐")
-        .replaceAll("с", "뜑").replaceAll("т", "뜒")
-        .replaceAll("у", "뜓").replaceAll("ф", "뜔")
-        .replaceAll("х", "뜕").replaceAll("ц", "뜞")
-        .replaceAll("ч", "뜖").replaceAll("ш", "뜗")
-        .replaceAll("щ", "뜘").replaceAll("ъ", "뜝")
-        .replaceAll("ы", "뜙").replaceAll("ь", "뜝")
-        .replaceAll("э", "뜚").replaceAll("ю", "뜛")
-        .replaceAll("я", "뜜");
-}
-  
-function copyToClipboard(input) {
-    const str = document.getElementById(input).value;
-    const result = replace(str);
 
-    if (!navigator.clipboard) {
-        fallbackCopyTextToClipboard(result);
-        return;
-    }
-
-    navigator.clipboard.writeText(result).then(function() {
-        document.getElementById("copied").innerHTML = "Текст скопирован!";
-        function clearAlert() {
-            document.getElementById("copied").innerHTML = "";
-        }
-        setTimeout(clearAlert, 3000);
-        console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-        console.error('Async: Could not copy text: ', err);
+function processCopy() {
+    copyToClipboard(replace(elementText('input')), function() {
+        const copiedElement = document.getElementById("copied");
+        copiedElement.innerHTML = "Текст скопирован!";
+        setTimeout(() => {
+            copiedElement.innerHTML = "";
+        }, 3000);
     });
 }
-  
